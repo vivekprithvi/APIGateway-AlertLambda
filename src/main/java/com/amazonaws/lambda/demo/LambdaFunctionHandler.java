6296edce33fbf4f48c0e20250989ef4b74063c0a
vulchivivek <vvulchi@gmu.edu>
2017-09-29 10:37:37 -0700

    First commit

---
+++ src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
@@ -0,0 +1,18 @@
+package com.amazonaws.lambda.demo;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LambdaFunctionHandler implements RequestHandler<String, String> {
+
+    @Override
+    public String handleRequest(String input, Context context) {
+        context.getLogger().log("Input: " + input);
+
+        if(input.contains("help"))
+        	return "Help Alert";
+        else
+        	return "Hello from Lambda!";
+    }
+
+}
